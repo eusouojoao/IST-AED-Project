172ba0d385e3e7f1d010839d2db783c0e18dcf1c
danieldinis007 <81173923+danieldinis007@users.noreply.github.com>
2021-10-04 18:36:44 +0100

    Add files via upload

--- graph.c
+++ graph.c
@@ -1,18 +1,21 @@
 #include <stdlib.h>
 #include "graph.h"
 
-typedef struct square square;
+typedef struct edge Edge;
+
 struct square
 {
     int p;      //p é a posição  do vértice no tabuleiro
     int weight; //peso do vertice (para transpôr) (ver se faz sentido)
-    square *next;
+    Square *next;
 };
 struct graph
 {
     int V;   //nº total de vértices
+    int L;   //nºde linhas
+    int C;   //nºde colunas
     float E; //nº total de arestas (nem sempre está com o valor atualizado)
-    square **adj;
+    Square **adj;
 };
 struct edge
 {
@@ -21,42 +24,104 @@ struct edge
     int weight; //peso da aresta (ver se faz sentido)
 };
 
-square *newSquare(int p, int weight, square *next)
+Edge *newEdge(int v, int w, int weight)
 {
-    square *newS = (square *)malloc(sizeof(struct square));
+    Edge *newE = (Edge *)malloc(sizeof(struct edge));
+    newE->v = v;
+    newE->w = w;
+    newE->weight = weight;
+    return newE;
+}
+
+Square *newSquare(int p, int weight)
+{
+    Square *newS = (Square *)malloc(sizeof(struct square));
+    newS->p = p;
+    newS->weight = weight;
+    newS->next = NULL;
+    return newS;
+}
+
+/*Square *newS(int p, int weight, Square *next)
+{
+    Square *newS = (Square *)malloc(sizeof(struct square));
     newS->p = p;
     newS->weight = weight;
     newS->next = next;
     return newS;
+} */
+
+Square *addS(Square *headS, Square *newS)
+{
+    if (headS == NULL)
+    {
+        headS = newS;
+    }
+    newS->next = headS->next;
+    headS->next = newS;
+    return headS;
 }
 
-Graph *GraphInit(int V)
+Graph *graphInit(int V, int L, int C, int E)
 {
     int v;
     Graph *newG = (Graph *)malloc(sizeof(struct graph));
     newG->V = V;
-    newG->E = 0;
-    newG->adj = (square **)malloc(V * sizeof(square *));
+    newG->L = L;
+    newG->C = C;
+    newG->E = E;
+    newG->adj = (Square **)malloc(V * sizeof(Square *));
     for (v = 0; v < V; v++)
         newG->adj[v] = NULL;
     return newG;
 }
 
-void GraphInsertS(Graph *G, Edge *e)
+int adjacentTile(int tile, int l, int c, int HouV) //HouV- foi realizada soma horizontal ou vertical
 {
-    int v = e->v, w = e->w, weight = e->weight;
-    G->adj[v] = newSquare(w, weight, G->adj[v]); //tamos a supôr que não são inseridas variáveis repetidas
-    G->adj[w] = newSquare(v, weight, G->adj[w]);
-    G->E++;
+    if (HouV == 1) //HouV==1 soma horizontal
+    {
+        if ((tile % 6) == 0)
+            return 0; //não é adjacente
+    }
+    else if (tile < 0 || tile > (l * c - 1))
+        return 0; //não é adjacente
+    return 1;
 }
 
-int GraphEdges(Edge **EdgesArray, Graph *G)
+void graphInsertS(Graph *G, Square *newS)
+{
+    int v = newS->p;
+    Square *headS = G->adj[v];
+    G->adj[v] = addS(headS, newS);
+    if (adjacentTile(v + 1, G->L, G->C, 1) == 1) //colocar a adjacencia do square na lista
+    {
+        headS = G->adj[v + 1];
+        G->adj[v + 1] = addS(headS, newS);
+    }
+    if (adjacentTile(v - 1, G->L, G->C, 1) == 1)
+    {
+        headS = G->adj[v - 1];
+        G->adj[v - 1] = addS(headS, newS);
+    }
+    if (adjacentTile(v + G->C, G->L, G->C, 0) == 1)
+    {
+        headS = G->adj[v + G->C];
+        G->adj[v + G->C] = addS(headS, newS);
+    }
+    if (adjacentTile(v - G->C, G->L, G->C, 0) == 1)
+    {
+        headS = G->adj[v - G->C];
+        G->adj[v - G->C] = addS(headS, newS);
+    }
+}
+
+int graphEdges(Edge **edgesArray, Graph *G)
 {
     int i, E = 0;
-    square *t;
+    Square *t;
     for (i = 0; i < G->V; i++)
-        for (t = G->adj; t != NULL; t = t->next)
+        for (t = G->adj[i]; t != NULL; t = t->next)
             if (i < t->p)
-                EdgesArray[E++] = newEdge(i, t->weight, t->p);
+                edgesArray[E++] = newEdge(i, t->weight, t->p);
     return E;
 }
--- graph.h
+++ graph.h
@@ -1,9 +1,12 @@
-typedef struct edge Edge;
-Edge *newEdge(int, int, int); //função que faz uma aresta entre dois numeros (recebe os parametrs do utilizador)
+typedef struct square Square;
+Square *newSquare(int, int); //função que faz uma aresta entre dois numeros (recebe os parametrs do utilizador)
 
 typedef struct graph Graph;
 
-Graph *GraphInit(int);              //cria um grafo com um numero de arestas
-void GraphInsertS(Graph *, Edge *); //insere uma aresta no grafo
+//typedef struct edge Edge;
+
+Graph *graphInit(int, int, int, int); //cria um grafo com um numero de arestas
+void graphInsertS(Graph *, Square *); //insere uma aresta no grafo
+
 //void GraphRemoveS(Graph *, Edge *); //não deve ser preciso remover quadrados
-int GraphEdges(Edge **, Graph *); //diz o numero de quadrados e coloca as arestas num vetor
+//int graphEdges(Edge **, Graph *); //diz o numero de quadrados e coloca as arestas num vetor
